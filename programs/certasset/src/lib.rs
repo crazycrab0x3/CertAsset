375bb5af29c700e7e61234b928e5d43368348819
crazycrab0x3 <ichirohasegawa0x3@gmail.com>
2023-02-16 17:24:49 -0500

    Feat: Smart Contract State Layout

--- programs/certasset/src/lib.rs
+++ programs/certasset/src/lib.rs
@@ -6,10 +6,37 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod certasset {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    /// Allows the user to create a certification request
+    pub fn create_request(ctx: Context<CreateSR>, authority: Pubkey, uri: String) -> Result<()> {
+        Ok(())
+    }
+
+    /// Allows the Signer Authority to Sign a Certification Request
+    pub fn sign_certificate(ctx: Context<SignRequest>) -> Result<()> {
         Ok(())
     }
 }
 
+// Instructions
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateSR<'info> {
+    applicant: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct SignRequest<'info> {
+    request: Account<'info, SigningRequest>,
+    signer: Signer<'info>
+}
+
+
+// Accounts
+
+#[account]
+pub struct SigningRequest {
+    applicant: Pubkey,
+    authority: Pubkey,
+    uri: String,
+    signed: bool
+}
