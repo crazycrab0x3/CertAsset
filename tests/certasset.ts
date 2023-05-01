5a6fa843c893841a3a19b7429103d7794886f13c
crazycrab0x3 <ichirohasegawa0x3@gmail.com>
2023-05-01 16:26:11 -0500

    Feat: Restructure request

--- tests/certasset.ts
+++ tests/certasset.ts
@@ -2,56 +2,68 @@ import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { assert } from "chai";
 import { Certasset } from "../target/types/certasset";
+import { Connection } from "@solana/web3.js";
 
-describe("certasset", () => {
+describe("certasset", async () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.Certasset as Program<Certasset>;
   const provider = anchor.AnchorProvider.local();
 
+  // Creates Authority and Applicant
+  console.log("Generating Testing Keypairs ...");
+
+  const authority = anchor.web3.Keypair.generate();
+  console.log("Generated Authority: " + authority.publicKey.toString());
+
+  const applicant = provider.wallet;
+  console.log("Generated Applicant: " + applicant.publicKey.toString());
+
+  const request_key = anchor.web3.Keypair.generate();
+  console.log("Generated Request Key: " + request_key.publicKey.toString());
+
+  console.log("Requesting Airdrop for Authority Test Wallet")
+  const airdrop_tx = await provider.connection.requestAirdrop(
+    authority.publicKey,
+    10000000000
+  );
+  console.log("Successfully Requested with TX ID: " + airdrop_tx);
+
   it("Ping the Program", async () => {
     console.log("Pinging Program ...");
     await program.methods.ping().rpc();
-    console.log("Succesfull Ping!")
-  })
+    console.log("Succesfull Ping!");
+  });
 
   it("Creates a Signing Request", async () => {
-    // Creates Authority and Applicant
-    console.log("Generating Testing Keypairs ...");
-    
-    const authority = anchor.web3.Keypair.generate();
-    console.log("Generated Authority: " + authority.publicKey.toString());
-
-    const applicant = provider.wallet;
-    console.log("Generated Applicant: " + applicant.publicKey.toString());
-
-    const request_key = anchor.web3.Keypair.generate();
-    console.log("Generated Request Key: " + request_key.publicKey.toString());
-
     // Call the smart contract
-
-    const tx = await program.methods.createRequest(authority.publicKey, "hola mundo")
+    const tx = await program.methods
+      .createRequest(authority.publicKey, "hola mundo")
       .accounts({
         request: request_key.publicKey,
         applicant: applicant.publicKey,
-        systemProgram: anchor.web3.SystemProgram.programId
+        systemProgram: anchor.web3.SystemProgram.programId,
       })
       .signers([request_key])
       .rpc();
     console.log("Your transaction signature", tx);
 
-    let signingRequest = await program.account.signingRequest.fetch(request_key.publicKey);
+    let signingRequest = await program.account.signingRequest.fetch(
+      request_key.publicKey
+    );
 
     assert.ok(signingRequest.applicant.equals(applicant.publicKey));
     assert.ok(signingRequest.authority.equals(authority.publicKey));
     assert.isFalse(signingRequest.signed);
     assert.equal(signingRequest.uri, "hola mundo");
+  });
 
+  it("Certificates a Signing Request", async () => {
     const [mint_key, _bump] = anchor.web3.PublicKey.findProgramAddressSync(
       [
         anchor.utils.bytes.utf8.encode("certasset-rq"),
-        request_key.publicKey.toBuffer()
+        request_key.publicKey.toBuffer(),
       ],
       program.programId
     );
@@ -70,7 +82,7 @@ describe("certasset", () => {
       })
       .signers([authority])
       .rpc();
-    
+
     let signedRequest = await program.account.signingRequest.fetch(
       request_key.publicKey
     );
